b331bf8770578f49b5c0fe3aaef82353a5f232cc
Jul1nk <55949423+Jul1nk@users.noreply.github.com>
2024-03-05 13:34:53 +0100

    Player mouse

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,27 +1,30 @@
 extends CharacterBody2D
 
+const SPEED = 150.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var mouse_pos = Vector2.ZERO
+var direction = Vector2.RIGHT
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 0
+@onready var mouse_sprite = $mouse_sprite
+@onready var rotatage = $rotatage
 
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
 
+func _process(_delta):
+	mouse_pos = get_local_mouse_position()
+	mouse_sprite.position = mouse_pos
+	
+	rotatage.rotation = atan2(mouse_pos.y, mouse_pos.x)
+
+
+
+func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Vector2(Input.get_axis("ui_left", "ui_right"),Input.get_axis("ui_up", "ui_down"))
-	if direction:
-		velocity = direction * SPEED
+	var mvt_direction = Vector2(Input.get_axis("left", "right"),Input.get_axis("up", "down"))
+	if mvt_direction:
+		velocity = mvt_direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
