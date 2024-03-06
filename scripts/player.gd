6045444e787a809fbeb1be2fecd1209c92e61cb0
Jul1nk <55949423+Jul1nk@users.noreply.github.com>
2024-03-06 12:27:56 +0100

    Little cleaning + code comment

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,34 +1,36 @@
 extends CharacterBody2D
 
+
 const SPEED = 150.0
+
 
 var mouse_pos = Vector2.ZERO
 var direction = Vector2.RIGHT
 
+
 @onready var mouse_sprite = $mouse_sprite
 @onready var rotatage = $rotatage
-
 @onready var sprite = $sprite
 @onready var flashlight = $rotatage/Flashlight
 
 
 func _process(_delta):
+		#Handling action keys
 	if Input.is_action_just_pressed("flashlight"):
 		flashlight.visible = !flashlight.visible
 
 
 func _physics_process(_delta):
+		#Handling mouse position and character rotation
 	mouse_pos = get_local_mouse_position()
 	mouse_sprite.position = mouse_pos
-	
 	rotatage.rotation = atan2(mouse_pos.y, mouse_pos.x)
-	
+	#Rotating canvases for player sprite
 	for canvas_layer in sprite.get_children():
 		canvas_layer.rotation = rotatage.rotation + PI
 		canvas_layer.offset = position
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Basic 2D movement
 	var mvt_direction = Vector2(Input.get_axis("left", "right"),Input.get_axis("up", "down"))
 	if mvt_direction:
 		velocity = mvt_direction * SPEED
--- scripts/wall_maker.gd
+++ scripts/wall_maker.gd
@@ -1,37 +1,45 @@
 extends Node2D
 
+
 @export var nb_layers = 5
 @export var layer_distance = 5
-
 @export var colors: Array[Color]
 
 @onready var input = $Input
-@onready var output = $Output
-
 @onready var tilemap = $Input/CanvasLayer/TileMap
 @onready var canvas_modulate = $Input/CanvasModulate
 
-# Called when the node enters the scene tree for the first time.
+@onready var output = $Output
+
+#How to make walls with the Wall Maker:
+#Place your walls with the white tiles of the Tilemap (Input/CanvasLayer/Tilemap)
+#The Wall Maker will automatically create a number of CanvasLayers and their zoom level to create a pseudo-3D effect
+
+
 func _ready():
-	
 	var current_distance = 1.0
 	
+		#Creation of the layers
 	for i in range(nb_layers):
+		#New layer
 		var canvas_i = CanvasLayer.new()
 		var tilemap_i = tilemap.duplicate()
 		var canvas_mod_i = canvas_modulate.duplicate()
 		
+		#CanvasLayer setup
 		canvas_i.follow_viewport_enabled = true
 		canvas_i.follow_viewport_scale = current_distance
-		print(current_distance)
 		current_distance += layer_distance *0.01
 		
+		#If Tilemap is last, make it emit shadows
 		if i == nb_layers-1:
 			tilemap_i.tile_set = load("res://editor/wall_tileset_shadow.tres")
 		tilemap_i.modulate = colors[i%nb_layers]
 		
+		#Output tree setup
 		canvas_i.add_child(tilemap_i)
 		canvas_i.add_child(canvas_mod_i)
 		output.add_child(canvas_i)
 	
+	#Delete Input
 	input.queue_free()
