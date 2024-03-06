0207ce10eeeeae546db72810c9ea27a97c6e6434
Jul1nk <55949423+Jul1nk@users.noreply.github.com>
2024-03-06 12:20:51 +0100

    Fog shader

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,10 +8,13 @@ var direction = Vector2.RIGHT
 @onready var mouse_sprite = $mouse_sprite
 @onready var rotatage = $rotatage
 
+@onready var sprite = $sprite
+@onready var flashlight = $rotatage/Flashlight
 
 
-
-
+func _process(_delta):
+	if Input.is_action_just_pressed("flashlight"):
+		flashlight.visible = !flashlight.visible
 
 
 func _physics_process(_delta):
@@ -19,6 +22,10 @@ func _physics_process(_delta):
 	mouse_sprite.position = mouse_pos
 	
 	rotatage.rotation = atan2(mouse_pos.y, mouse_pos.x)
+	
+	for canvas_layer in sprite.get_children():
+		canvas_layer.rotation = rotatage.rotation + PI
+		canvas_layer.offset = position
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
