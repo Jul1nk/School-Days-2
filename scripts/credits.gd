5cf9c6dbff3949477318f480cd40cf635f15c5b8
Jul1nk <55949423+Jul1nk@users.noreply.github.com>
2024-03-14 00:22:33 +0100

    Credits

---
+++ scripts/credits.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.connect("timeout", finish)
+	$Timer.start()
+	$AnimationPlayer.play("credits")
+
+func finish():
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
